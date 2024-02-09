853231fd994160ea95d8b4aad19fbc2fdf7d1a15
Amit Dubey <adubey@cynergybank.co.uk>
2024-02-09 18:16:28 +0000

    Added Data Table Component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,46 @@
 import PageLayout from "../lib/components/page-layout";
+import DataTable from "react-data-table-component";
+
+// A super simple expandable component.
+const ExpandedComponent = ({ data }: { data: any }) => (
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+);
 
 const Home = () => {
+  const columns = [
+    {
+      name: "Title",
+      selector: (row: any) => row.title,
+    },
+    {
+      name: "Year",
+      selector: (row: any) => row.year,
+      sortable: true,
+    },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      title: "Beetlejuice",
+      year: "1988",
+    },
+    {
+      id: 2,
+      title: "Ghostbusters",
+      year: "1984",
+    },
+  ];
+
   return (
     <PageLayout>
-      <h1>This is home</h1>
+      <DataTable
+        columns={columns}
+        data={data}
+        selectableRows
+        expandableRows
+        expandableRowsComponent={ExpandedComponent}
+      />
     </PageLayout>
   );
 };
