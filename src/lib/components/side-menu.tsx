97eb6259fdd42fdcd481b5adf933440187c5012f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-11 00:29:07 +0530

    add icons

--- src/lib/components/side-menu.tsx
+++ src/lib/components/side-menu.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from "react";
-import { PageRoutes } from "../constants";
 import { Link } from "react-router-dom";
+import { MdSpaceDashboard } from "react-icons/md";
+import { LiaFileInvoiceSolid } from "react-icons/lia";
+import { PiSignOutBold } from "react-icons/pi";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { PageRoutes } from "../constants";
 
 interface SideMenuProps {
   children?: React.ReactNode;
@@ -13,21 +16,32 @@ const SideMenu: FC<SideMenuProps> = ({ children }: SideMenuProps) => {
       <div className="p-3 mb-4">
         <h4 className="mb-4">eMarket</h4>
       </div>
-      <Menu>
+      <Menu
+        menuItemStyles={{
+          button: ({ active }) => ({
+            backgroundColor: active ? "#333" : "transparent",
+            color: active ? "white" : "inherit",
+            transition: "background-color 0.3s",
+          }),
+        }}
+      >
         <MenuItem>
           <Link className="nav-link active" to={PageRoutes.Home}>
+            <MdSpaceDashboard />
             <span className="nav-link-text ms-1">Dashboard</span>
           </Link>
         </MenuItem>
         <MenuItem>
           <Link className="nav-link active" to={PageRoutes.Invoices}>
+            <LiaFileInvoiceSolid />
             <span className="nav-link-text ms-1">Invoices</span>
           </Link>
         </MenuItem>
 
         <MenuItem>
           <Link className="nav-link" to={PageRoutes.Login}>
-            <span className="nav-link-text ms-1">Sign Up</span>
+            <PiSignOutBold />
+            <span className="nav-link-text ms-1">Sign Out</span>
           </Link>
         </MenuItem>
       </Menu>
