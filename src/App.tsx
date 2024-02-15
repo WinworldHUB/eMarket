7132572fecc4a319ec9d967dcd72392062516f9e
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-15 16:29:39 +0530

    display invoice table

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import SignIn from "./pages/signin";
 import { PageRoutes } from "./lib/constants";
-import Dashboard from "./pages/dashboard";
+// import Dashboard from "./pages/dashboard";
 import Invoices from "./pages/Invoices";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PageRoutes.Home} element={<Dashboard />} />
+        {/* <Route path={PageRoutes.Home} element={<Dashboard />} /> */}
         <Route path={PageRoutes.Login} element={<SignIn />} />
         <Route path={PageRoutes.Invoices} element={<Invoices />} />
       </Routes>
--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import PageLayout from '../lib/components/page-layout'
+import InvoiceTable from '../lib/components/InvoiceTable'
 
 const Invoices = () => {
   return (
     <div>
-      
+      <PageLayout isShowSideMenu>
+        <InvoiceTable/>
+      </PageLayout>
     </div>
   )
 }
